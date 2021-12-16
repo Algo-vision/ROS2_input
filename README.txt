0c2da4d0dafd9ea103b274626a087fb1084f93ab
sharon <sharon@nanovel.co.il>
2021-12-16 15:10:53 +0200

    Added all files needed for installation and safe running.

---
+++ README.txt
@@ -0,0 +1,1 @@
+using as import routine
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='import_parents',
+      version='1.0',
+      py_modules=['import_parents'],
+      author='Mark Levin',
+      author_email='levmarki@gmail.com',
+      url='facebook.com',
+      )
